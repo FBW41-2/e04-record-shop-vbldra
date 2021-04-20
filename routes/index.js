4f779b41959794802997362095f0d1c8647cc618
ilona <ilona.avdiushkina@gmail.com>
2021-04-20 14:16:02 +0200

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+const records = [
+    {
+        "name": "Metallica",
+        "title": "Unforgiven",
+        "year": "1998"
+    },
+    {
+        "name": "Beatles",
+        "title": "Let it be",
+        "year": "1975"
+    },
+    {
+        "name": "System of a Down",
+        "title": "Chop Suey",
+        "year": "2002"
+    }
+]
+
+router.get('/api/records', (req, res, next) => {
+    res.json(records)
+})
+router.post('/api/records', (req, res, next) => {
+    records.push({
+        "name": req.body.name,
+        "title": req.body.title,
+        "year": req.body.year
+    })
+    res.redirect('/api/records')
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
