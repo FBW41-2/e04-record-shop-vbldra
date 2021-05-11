d9b4a77692c8e5ff53d16760b37a87712477dc94
ilona <ilona.avdiushkina@gmail.com>
2021-05-11 11:23:59 +0200

    added validation for users

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
+
+const userValidators = [
+    body("email").isEmail().normalizeEmail().withMessage("This is not a valid email"),
+    body("password")
+        .isStrongPassword({
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+            returnScore: true,
+            pointsPerUnique: 1,
+            pointsPerRepeat: 0.5,
+            pointsForContainingLower: 10,
+            pointsForContainingUpper: 10,
+            pointsForContainingNumber: 10,
+            pointsForContainingSymbol: 10,
+        })
+        .withMessage("Find better password"),
+];
 
 const {
-  getUsers,
-  getUser,
-  updateUser,
-  deleteUser,
-  addUser
+    getUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+    addUser,
 } = require("../controllers/usersController");
 
-router
-  .route("/")
-  .get(getUsers)
-  .post(addUser);
+router.route("/").get(getUsers).post(userValidators, addUser);
 
-router
-  .route("/:id")
-  .get(getUser)
-  .delete(deleteUser)
-  .put(updateUser);
+router.route("/:id").get(getUser).delete(deleteUser).put(updateUser);
 
 module.exports = router;
